761085584680360ec7d0d552de496312d1cc3a2d
Alexis <alexisdavalos.tech@gmail.com>
2020-02-14 11:15:46 -0500

    server.js | index.js initialized

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors')
+const server = express();
+//import routes
+
+//import database
+
+
+//middleware
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+// routes - endpoints
+
+
+// default base url http://localhost:5000 from index.js
+server.get('/', logger,  (req, res) => {
+  res.send(`<h2>Node Sprint Challenge!</h2>`);
+});
+
+module.exports = server;
+
+//custom middleware
+
+function logger(req, res, next) {
+  (req.params.id) ? 
+  console.log(`${req.method} Request to ${req.originalUrl} at: ${Date.now()} includes id:${req.params.id}`) :
+  console.log(`${req.method} Request to ${req.originalUrl} at: ${Date.now()} `); //requests url of request method
+  next(); //moves on
+}
+
